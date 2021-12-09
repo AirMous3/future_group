65669e9b0eba8ad6792d6018615cc434b167ecf5
AirMous3 <airmouselol@gmail.com>
2021-12-09 04:04:54 +0300

    add header

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,40 @@
 import React, { ReactElement } from 'react';
 
-export const App = (): ReactElement => <div>123</div>;
+import { Input, Select } from 'antd';
+
+import s from './app.module.scss';
+
+import 'antd/dist/antd.css';
+
+const { Search } = Input;
+const { Option } = Select;
+
+export const App = (): ReactElement => (
+  <div className={s.container}>
+    <div className={s.header}>
+      <h1>Search for books</h1>
+      <Search placeholder="input search text" allowClear style={{ width: 500 }} />
+      <div className={s.categoriesWrapper}>
+        <div className={s.categories}>
+          <div>categories</div>
+          <Select defaultValue="all" style={{ width: 130 }}>
+            <Option value="all">all</Option>
+            <Option value="art">art</Option>
+            <Option value="biography">biography</Option>
+            <Option value="computers">computers</Option>
+            <Option value="history">history</Option>
+            <Option value="medical">medical</Option>
+            <Option value="poetry">poetry</Option>
+          </Select>
+        </div>
+        <div className={s.categories}>
+          <div>sorting by</div>
+          <Select value="relevance" style={{ width: 130 }}>
+            <Option value="relevance">relevance</Option>
+            <Option value="newest">newest</Option>
+          </Select>
+        </div>
+      </div>
+    </div>
+  </div>
+);
